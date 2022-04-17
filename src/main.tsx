a8af705ca2c24f7a734a250fe11e987ef4eabf5e
ayuzaka <ayuzaka.f@gmail.com>
2022-04-17 14:36:34 +0900

    feat: add react-router tutorial

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import Expenses from './routes/expenses';
+import Invoices from './routes/invoices';
+import Invoice from './routes/invoice';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/expenses" element={<Expenses />} />
+          <Route path="/invoices" element={<Invoices />}>
+            <Route
+              index
+              element={
+                <main style={{ padding: '1rem' }}>
+                  <p>Select at invoice</p>
+                </main>
+              }
+            />
+            <Route path=":invoiceId" element={<Invoice />} />
+          </Route>
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: '1rem' }}>
+                <p>There's nothing else!</p>
+              </main>
+            }
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
